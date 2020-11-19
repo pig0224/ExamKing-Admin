3661c46cc98b80e742644a9eb66001c36c24306c
cjs <1459321586@qq.com>
2020-11-19 10:33:54 +0800

    [新增] 教师管理

--- src/views/teacher/create.vue
+++ src/views/teacher/create.vue
@@ -1,19 +1,115 @@
 <!-- teacher create -->
 <template>
-  <div class='teacher-create'>
-    teacher create
+  <div class="app-container">
+    <el-form :model="form"
+             ref="form"
+             label-width="100px"
+             v-loading="formLoading"
+             :rules="rules">
+      <el-form-item label="所属系别："
+                    prop="deptId">
+        <dept-class-select :dept-id.sync="form.deptId"
+                           :hidden-classes="true"></dept-class-select>
+      </el-form-item>
+
+      <el-form-item label="教师姓名："
+                    prop="teacherName">
+        <el-input v-model="form.teacherName"></el-input>
+      </el-form-item>
+
+      <el-form-item label="教师性别：">
+        <sex-select :sex.sync="form.sex"></sex-select>
+      </el-form-item>
+
+      <el-form-item label="联系电话："
+                    prop="telphone">
+        <el-input v-model="form.telphone"></el-input>
+      </el-form-item>
+
+      <el-form-item label="教师工号："
+                    prop="teacherNo">
+        <el-input v-model="form.teacherNo"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码："
+                    prop="password">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+
+      <el-form-item label="身份证号码："
+                    prop="idCard">
+        <el-input v-model="form.idCard"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="
+                   primary"
+                   @click="submitForm">提交</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import teacherApi from '@/api/teacher'
+import DeptClassSelect from '@/components/DeptClassSelect'
+import SexSelect from '@/components/SexSelect'
+
 export default {
-  components: {},
+  components: { DeptClassSelect, SexSelect },
   data() {
-    return {}
+    return {
+      form: {
+        teacherName: '',
+        sex: '',
+        telphone: '',
+        teacherNo: '',
+        password: '',
+        deptId: 0,
+        idCard: '',
+      },
+      formLoading: false,
+      rules: {
+        deptId: [
+          { required: true, message: '请输入系别名称', trigger: 'blur' },
+        ],
+        teacherName: [
+          { required: true, message: '请输入11名称', trigger: 'blur' },
+        ],
+      },
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    async submitForm() {
+      let _this = this
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.formLoading = true
+          teacherApi
+            .create(this.form)
+            .then(() => {
+              _this.$message.success('新增成功')
+              _this.delCurrentView(_this).then(() => {
+                _this.$router.push('/teacher/list')
+              })
+            })
+            .catch(() => {
+              _this.formLoading = false
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    resetForm() {
+      this.$refs['form'].resetFields()
+    },
+    ...mapActions('tagsView', { delCurrentView: 'delCurrentView' }),
+  },
   created() {},
   mounted() {},
   beforeCreate() {}, //生命周期 - 创建之前
